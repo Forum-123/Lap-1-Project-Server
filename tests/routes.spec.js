d254fefcf6d947ec37b1aed17f0c953dc58b8d60
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-10-05 11:55:11 +0100

    added successful test for post route

--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
-const app = require('../server.js')
+const app = require('../server.js');
+const data = require('../data')
 
 describe('APi routes', (done) => {
 
@@ -40,27 +41,32 @@ describe('APi routes', (done) => {
 
     test('Get all comments on a particular entry', () => {
         request(app)
-            .get('/entries/comments/2')
+            .get('/entries/comments/1')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            .expect([{ id: 1, text: "", author: "" }])
             .expect(200, done);
 
     })
-
-    // test(" Post new comment on a particular entry", (done) => {
-    //     request(api)
-    //         .post('/entries/comments/:id')
-    //         .send()
-    //         .set('Accept', 'application/json') //set - will set what we expect to get from this
-    //         .expect('content-Type', /json/) // expect to get back some json
-    //         .expect()
-    //         .expect(201, done) // 201 = status code when something is successfully created
-    // })
-
-
-
+    // test Post route when posting a new comment on a particular entry
+    test(" Post new comment on a particular entry", (done) => {
+        let testComments =
+        {
+            text: "I feel like starting an argument, so: What actor, living or dead, any gender, was the greatest combination of acting talent, physical beauty and pure star-power charisma?",
+            author: "Scott Renshaw"
+        }
+        request(api)
+            .post('/entries/comments/1')
+            .send(testComments)
+            .set('Accept', 'application/json') //set - will set what we expect to get from this
+            .expect('content-Type', /json/) // expect to get back some json
+            .expect({ id: 2, ...testComments })
+            .expect(201, done) // 201 = status code when something is successfully created
+    })
 })
+
+
+
+
 
 
 
