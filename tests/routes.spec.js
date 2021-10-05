10eedd596d0ee1fd7a5a0f013e09e302f1dadef9
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-10-05 12:58:15 +0100

    added test for error handling

--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -18,8 +18,8 @@ describe('APi routes', (done) => {
         api.close(done)
     })
 
-
-    test('Get all entries', () => {
+    //All Entries
+    test('Get all entries', (done) => {
         request(app)
             .get('/entries')
             .set('Accept', 'application/json')
@@ -27,25 +27,38 @@ describe('APi routes', (done) => {
             .expect(200, done);
 
     })
-
-    test('Get all entries by a particular user', () => {
+    // Entries by a particular user
+    test('Get entries by a particular user', (done) => {
         request(app)
-            .get('/entries/:username')
+            .get('/entries/BBC News (UK)')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200, done);
 
     })
+    // Testing Error handling of Entries of an unknown user
+    test('Error handling when Get all entries by a particular user', (done) => {
+        request(app)
+            .get('/entries/BBC')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(404, done);
 
-
-
-    test('Get all comments on a particular entry', () => {
+    })
+    // comments 
+    test('Get all comments on a particular entry', (done) => {
         request(app)
             .get('/entries/comments/1')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200, done);
 
+    })
+    // testing error handling of out of index comment
+    test("out of index comment is handled", (done) => {
+        request(api)
+            .get("/entries/comments/0")
+            .expect({ "message": "Entry of id 0 not found" }, done);
     })
     // test Post route when posting a new comment on a particular entry
     test(" Post new comment on a particular entry", (done) => {
@@ -62,6 +75,8 @@ describe('APi routes', (done) => {
             .expect({ id: 2, ...testComments })
             .expect(201, done) // 201 = status code when something is successfully created
     })
+
+
 })
 
 
