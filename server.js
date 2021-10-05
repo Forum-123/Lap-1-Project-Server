c8cc70741575cbff7d4ad2a97271652a14ec2820
Forum Desai <forumd1999@gmail.com>
2021-10-05 17:55:30 +0100

    Clean up code and change 'gif' back to 'gifUrl'

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const Entry = require('./models/entry'); // Obtain the Entry class
+const Entry = require('./models/entry');
 
 const app = express();
 
@@ -8,22 +8,21 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    try {    
+    try {
         res.json('Server is running');
     } catch {
-        res.status(500).json({ message: "Internal server error" })
+        res.status(500).json({ message: 'Internal server error' })
     }
 });
 
 // Get all entries
 app.get('/entries', (req, res) => {
     const entriesArr = Entry.all;
-    try {    
+    try {
         res.json(entriesArr);
     } catch {
-        res.status(404).json({ message: "Data not found" })
+        res.status(404).json({ message: 'Data not found' })
     }
-
 });
 
 // Get all entries by a particular user
@@ -36,7 +35,7 @@ app.get('/entries/:username', (req, res) => {
             let index = entriesArr.indexOf(e);
             let entriesByUsername = Entry.getEntryByUsername(entriesArr[index].username);
             return res.send(entriesByUsername);
-        } 
+        }
     }
 
     res.status(404).json({ message: `Entry by username ${requestedUsername} not found` });
@@ -62,8 +61,8 @@ app.post('/entries', (req, res) => {
     const newData = req.body;
 
     // All fields are required
-    if (!newData.title || !newData.username || !newData.message || !newData.gif) {
-        return res.status(400).json({ message: 'Please fill in the required title, username, message and gif fields' });
+    if (!newData.title || !newData.username || !newData.message || !newData.gifUrl) {
+        return res.status(400).json({ message: 'Please fill in the required title, username, message and gifUrl fields' });
     } else {
         const newEntry = Entry.addEntry(newData);
         return res.status(201).send(newEntry);
@@ -99,7 +98,7 @@ app.put('/entries/:id', (req, res) => {
         }
     }
 
-     res.status(404).json({ message: `Entry of id ${requestedId} not found` })
+    res.status(404).json({ message: `Entry of id ${requestedId} not found` })
 });
 
 // Change reaction on an entry
@@ -107,19 +106,19 @@ app.put('/entries/reactions/:id', (req, res) => {
     const entriesArr = Entry.all;
     const requestedId = parseInt(req.params.id);
     const newReaction = req.body.reaction;
-  
+
     // object.hasOwnProperty('string') returns true if 'string' is a key in 'object'
     if (entriesArr[0].reactions.hasOwnProperty(`${newReaction}`)) {
         for (let e of entriesArr) {
             if (e.id === requestedId) {
                 Entry.changeReaction(requestedId, newReaction);
-                return res.status(201).json({ message: 'Reaction successfully updated'});
+                return res.status(201).json({ message: 'Reaction successfully updated' });
             }
         }
     } else {
         return res.status(400).json({ message: `${newReaction} is an invalid input` })
     }
-    
+
     res.status(404).json({ message: `Entry of id ${requestedId} not found` });
 });
 
